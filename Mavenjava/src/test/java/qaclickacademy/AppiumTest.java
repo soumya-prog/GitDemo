a01e888d6f186dd0c4699f4bc99dd91f666d2969
soumya-prog <soumya.ice.46@gmail.com>
2019-12-20 10:44:46 -0600

    Commit message

---
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void mobileapp() {
+		System.out.println("mobileapp");
+	}
+	@Test
+	public void addutil() {
+		System.out.println("addutil");
+	}
+
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/RestApItest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApItest {
+
+	@Test
+	public void postjira() {
+		System.out.println("postjira");
+	}
+	@Test
+	public void deleteutil() {
+		System.out.println("deleteutil");	
+	}
+}
