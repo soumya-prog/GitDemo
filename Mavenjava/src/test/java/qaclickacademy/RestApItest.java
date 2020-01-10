97f745aa3d4697b53a02a6f7d3e23aaefdfd3927
soumya <soumya.ice.46@gmail.com>
2020-01-09 21:11:08 -0600

    modified in restapi

--- Mavenjava/src/test/java/qaclickacademy/RestApItest.java
+++ Mavenjava/src/test/java/qaclickacademy/RestApItest.java
@@ -7,7 +7,10 @@ public class RestApItest {
 	@Test
 	public void postjira() {
 		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
 	}
+	
 	@Test
 	public void deleteutil() {
 		System.out.println("deleteutil");	
